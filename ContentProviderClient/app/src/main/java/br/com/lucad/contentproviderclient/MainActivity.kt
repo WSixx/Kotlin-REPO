43d3e8989c5f69c1da568d1cdd6f42d9dc8772d8
WSixx <lucas.2007s@gmail.com>
2021-04-19 13:39:41 -0300

    Notification

--- ContentProviderClient/app/src/main/java/br/com/lucad/contentproviderclient/MainActivity.kt
+++ ContentProviderClient/app/src/main/java/br/com/lucad/contentproviderclient/MainActivity.kt
@@ -1,10 +1,14 @@
 package br.com.lucad.contentproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.controls.actions.FloatAction
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +21,25 @@ class MainActivity : AppCompatActivity() {
 
         notesRefreshButton = findViewById(R.id.client_button_refresh)
         notesRecycle = findViewById(R.id.client_list)
+        getContentProvider()
 
         notesRefreshButton.setOnClickListener{
+            getContentProvider()
+        }
+    }
 
+    private fun getContentProvider(){
+        try {
+            val url = "content://br.com.lucad.contentprovider.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? =
+                contentResolver.query(data, null, null, null, "title")
+            notesRecycle.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        }catch (ex: Exception){
+            ex.printStackTrace()
         }
     }
 }
