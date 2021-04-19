29774a3877275cb974a692faa2d29b0adc897950
WSixx <lucas.2007s@gmail.com>
2021-04-19 13:01:46 -0300

    ContentProvider Client Adapter

--- ContentProviderClient/app/src/main/java/br/com/lucad/contentproviderclient/MainActivity.kt
+++ ContentProviderClient/app/src/main/java/br/com/lucad/contentproviderclient/MainActivity.kt
@@ -2,10 +2,24 @@ package br.com.lucad.contentproviderclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.controls.actions.FloatAction
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notesRecycle: RecyclerView
+    lateinit var notesRefreshButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notesRefreshButton = findViewById(R.id.client_button_refresh)
+        notesRecycle = findViewById(R.id.client_list)
+
+        notesRefreshButton.setOnClickListener{
+
+        }
     }
 }
