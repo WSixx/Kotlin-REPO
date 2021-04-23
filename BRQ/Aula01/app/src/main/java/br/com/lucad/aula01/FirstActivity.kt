725ecfc081b6b57fa99b776e0e4f85a20b75c888
WSixx <lucas.2007s@gmail.com>
2021-04-23 18:20:56 -0300

    Aula 01

---
+++ BRQ/Aula01/app/src/main/java/br/com/lucad/aula01/FirstActivity.kt
@@ -0,0 +1,11 @@
+package br.com.lucad.aula01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
