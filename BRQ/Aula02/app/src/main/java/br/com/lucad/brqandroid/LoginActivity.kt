bfc0768491346d4478f1b7a802f921573dcf0d4a
WSixx <lucas.2007s@gmail.com>
2021-04-28 10:17:45 -0300

    NotesSql

--- BRQ/Aula02/app/src/main/java/br/com/lucad/brqandroid/LoginActivity.kt
+++ BRQ/Aula02/app/src/main/java/br/com/lucad/brqandroid/LoginActivity.kt
@@ -1,8 +1,10 @@
 package br.com.lucad.brqandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,6 +14,9 @@ class LoginActivity : AppCompatActivity() {
 
         button_login.setOnClickListener {
             Toast.makeText(this,  "Button Login Clicked!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,LoginActivity2::class.java)
+            startActivity(intent)
+
         }
     }
 }
